6ec61941ef77bc460dbfc3f471815ec412779201
John White <750350+johnhwhite@users.noreply.github.com>
2021-09-30 15:19:32 -0400

    Merge 5.0.0 next (#65)
    * initial commit
    
    * 5.0.0-beta.0 release (#56)
    
    * Update libraries to 5
    
    * changelog
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * Next > Adopt Angular CLI (#59)
    
    * merge from master
    
    * eject
    
    * e2e tests
    
    * bumped indicators
    
    * package lock
    
    * Upgrade packages
    
    * ng update packages
    
    * Use partial mode
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    Co-authored-by: Blackbaud-SteveBrush <steve.brush@blackbaud.com>
    
    * Build #skyux-avatar-push-1194382227-90134: Added new baseline screenshots. [ci skip]
    
    * 5.0.0-beta.1 release; visual test fix (#60)
    
    * 5.0.0-beta.2 release
    
    * Update CHANGELOG.md
    
    Co-authored-by: John Lyons <John.Lyons@blackbaud.com>
    
    * e2e tweak
    
    * added noop styles to showcase
    
    * fix
    
    * Delete screenshots
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    Co-authored-by: John Lyons <John.Lyons@blackbaud.com>
    Co-authored-by: Blackbaud-SteveBrush <steve.brush@blackbaud.com>
    
    * Build #skyux-avatar-push-1195376206-93363: Added new baseline screenshots. [ci skip]
    
    * Added theme switcher; removed docs-tools; removed self references (#61)
    
    * Added theme switcher; removed docs-tools; removed self references
    
    * reverted baselines
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * 5.0.0-beta.2 release (#63)
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * 5.0.0-beta.3 release (#64)
    
    * 5.0.0-beta.3 release
    
    * Update CHANGELOG.md
    
    Co-authored-by: John Lyons <John.Lyons@blackbaud.com>
    
    * Update CHANGELOG.md
    
    Co-authored-by: John Lyons <John.Lyons@blackbaud.com>
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    Co-authored-by: John Lyons <John.Lyons@blackbaud.com>
    
    * Workflow and package updates
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    Co-authored-by: Alex <alex.kingman@blackbaud.com>
    Co-authored-by: Blackbaud-SteveBrush <steve.brush@blackbaud.com>
    Co-authored-by: Blackbaud Sky Build User <sky-build-user@blackbaud.com>
    Co-authored-by: John Lyons <John.Lyons@blackbaud.com>

---
+++ projects/avatar-showcase/src/app/visual/avatar/avatar-visual.component.ts
@@ -0,0 +1,13 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'avatar-visual',
+  templateUrl: './avatar-visual.component.html'
+})
+export class AvatarVisualComponent {
+
+  public name = 'Robert C. Hernandez';
+
+}
